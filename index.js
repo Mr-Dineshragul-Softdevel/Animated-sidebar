355e98fa914a45fec05ed332983358dc796b6d05
Mr-Dineshragul-softdevel <dineshragul752@gmail.com>
2024-03-10 21:00:36 +0530

    navbar is created

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const sideNav=document.querySelector('.side-nav');
+    const btnNav=document.querySelector('#btnNav');
+    const overlayScreen=document.querySelector('.nav-overlay')
+
+    btnNav.addEventListener('click',()=>{
+        sideNav.classList.add('open');
+    })
+
+    btnNav.addEventListener('click',()=>{
+        sideNav.classList.add('open');
+    })
+})
